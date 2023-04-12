7b01fdb3c8d2b08f52d7c344d10a8bf8f7d6a0ee
juwonju <jxwxnjx@gmail.com>
2023-04-12 17:42:51 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -21,5 +22,7 @@ public abstract class Item {
     private int price;
     private int stockQuantity;
 
-    private List<Category> categories;
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
 }
