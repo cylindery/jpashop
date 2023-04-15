0968a87df00f02de371b0cf1064b9fc2f81b8574
juwonju <jxwxnjx@gmail.com>
2023-04-15 20:06:11 +0900

    회원 등록 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
 @RestController
 @RequiredArgsConstructor
@@ -39,6 +40,7 @@ public class MemberApiController {
 
     @Data
     static class CreateMemberRequest {
+        @NotEmpty
         private String name;
     }
 
