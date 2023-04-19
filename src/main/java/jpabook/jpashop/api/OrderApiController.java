38e878019ed686567afc69477290bacc7c771d07
juwonju <jxwxnjx@gmail.com>
2023-04-19 17:51:17 +0900

    주문 조회 V4: JPA에서 DTO 직접 조회

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ import java.util.stream.Collectors;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     /**
      * V1. 엔티티 직접 노출
@@ -47,7 +50,7 @@ public class OrderApiController {
     }
 
     /**
-     * V3: 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
      */
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
@@ -60,7 +63,7 @@ public class OrderApiController {
     }
 
     /**
-     * V3.1: 엔티티를 조회해서 DTO로 변환 페이징 고려
+     * V3.1. 엔티티를 조회해서 DTO로 변환 페이징 고려
      */
     @GetMapping("/api/v3.1/orders")
     public List<OrderDto> ordersV3_page(
@@ -75,6 +78,21 @@ public class OrderApiController {
         return result;
     }
 
+    /**
+     * V4. JPA에서 DTO로 바로 조회, 컬렉션 N 조회 (1 + N Query)
+     */
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> orders4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    /**
+     * V5. JPA에서 DTO로 바로 조회, 컬렉션 1 조회 최적화 버전 (1 + 1 Query)
+     */
+
+    /**
+     * V6. JPA에서 DTO로 바로 조회, 플랫 데이터 (1 Query)
+     */
 
     @Data
     static class OrderDto {
